608a95aa7489301e7fba5d25d3cae6c928a766e7
Ignacio Acevedo <ig.acevedoe@duocuc.cl>
2026-06-03 20:55:08 -0400

    Se hacen algunas modificaciones

--- app.py
+++ app.py
@@ -3,10 +3,25 @@ from agent_engine import agent_answer
 
 st.set_page_config(
     page_title="Asistente DUOC UC",
-    page_icon="🎓"
+    page_icon="🎓",
+    layout="wide"
 )
 
 st.title("🎓 Asistente Virtual DUOC UC Plaza Oeste")
+
+st.markdown(""" Bienvenidos al asistende acádemico inteligente de DUOC UC!
+            
+            Adelante consulte sobre:
+            
+            - Matrículas
+            
+            - Becas
+            
+            - Reglamentos
+            
+            - Redacción de correos
+            
+            """)
 
 pregunta = st.text_input(
     "Ingrese su consulta académica"
